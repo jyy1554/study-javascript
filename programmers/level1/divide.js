2a9fce279f30882eb108602d1b2618f1c7db8239
jyy1554 <jyy1556@gmail.com>
2021-10-01 02:35:10 +0900

    최대공약수와 최소공배수 -  완2

--- programmers/level1/divide.js
+++ programmers/level1/divide.js
@@ -5,18 +5,17 @@
 //나의 풀이
 function solution(n, m) {
   var answer = [];
-  const bigger = n >= m ? n : m;
-  const smaller = n <= m ? n : m;
-  let divideBy = smaller;
+  let smaller = n <= m ? n : m; // n과 m 중 작은값
   
-  while (divideBy) {
-      if (!(bigger % divideBy) && !(smaller % divideBy)) { // 나누어 떨어질 때
-          answer.push(divideBy);
-          answer.push(bigger * smaller / divideBy);
+  while (smaller) {
+      if (!(n % smaller) && !(m % smaller)) { // 나누어 떨어질 때
+          answer.push(smaller);
+          answer.push(n * m / smaller);
           return answer;
       }
-      else {
-          divideBy--;
+      else { // 나누어 떨어지지 않는 경우, 나누는 값을 1씩 감소
+          smaller--;
       }
   }
+      
 }
