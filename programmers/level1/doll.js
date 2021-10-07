c75237d619ae98617f77e43f3b3116dcf10f0de7
jyy1554 <jyy1556@gmail.com>
2021-10-07 21:55:09 +0900

    크레인 인형뽑기 게임 - 완료2

--- programmers/level1/doll.js
+++ programmers/level1/doll.js
@@ -32,3 +32,30 @@ function solution(board, moves) {
   
   return answer;
 }
+
+
+// 다른 풀이 - 바구니에 넣기전에 동일한 인형 처리
+function solution(board, moves) {
+  var answer = 0;
+  var stack = [];
+  
+  while (moves.length) {
+      const move = moves.shift()-1;
+      for(let i=0; i < board.length; i++) {
+          if(board[i][move] != 0) {
+              if(stack[stack.length-1] == board[i][move]) {
+                  answer += 2;
+                  stack.pop();
+              }
+              else {
+                  stack.push(board[i][move]);
+              }
+              board[i][move] = 0;
+              break;
+          }
+      }
+  }
+  
+  
+  return answer;
+}
