49284cdb5de27b9825b98b85e5f624f1f1265e86
jyy1554 <jyy1556@gmail.com>
2021-10-08 16:48:59 +0900

    키패드 누르기  - 완료2

--- programmers/level1/keypad.js
+++ programmers/level1/keypad.js
@@ -2,6 +2,68 @@
 
 // 키패드 누르기
 
+/*나의 최종 풀이 */
+function numPosition(num) {
+  const numPos = [];
+  switch (num) {  // 행
+      case 2: numPos[0] = 0;
+          break;
+      case 5: numPos[0] = 1;
+          break;
+      case 8: numPos[0] = 2;
+          break;
+      case 0: numPos[0] = 3;
+          break;
+  }
+  numPos[1] = 1;  // 열
+
+  return numPos
+}
+
+function solution(numbers, hand) {
+  let result = '';
+  let leftPos = [3, 0];   // '*'
+  let rightPos = [3, 2];  // '#'
+  
+  // 위치 계산 함수
+  for(let i=0; i < numbers.length; i++) {
+      const num = numbers[i];
+      
+      if('147'.indexOf(num) != -1) {
+          leftPos[0] = num == 1 ? 0 : (num == 4 ? 1 : 2);
+          leftPos[1] = 0;
+          result += 'L';
+      }
+      else if('369'.indexOf(num) != -1) {
+          rightPos[0] = num == 3 ? 0 : (num == 6 ? 1 : 2);
+          rightPos[1] = 2;
+          result += 'R';
+      }
+      else {  //2580 크거나작거나 or 동일
+          const numPos = numPosition(num);
+          const leftDistance = Math.abs(numPos[0] - leftPos[0]) + Math.abs(numPos[1] - leftPos[1]);
+          const rightDistance = Math.abs(numPos[0] - rightPos[0]) + Math.abs(numPos[1] - rightPos[1]);
+          
+          if ((leftDistance < rightDistance) || (leftDistance == rightDistance && hand == 'left')) {
+              leftPos[0] = numPos[0];
+              leftPos[1] = numPos[1];
+              result += 'L';
+          }
+          else {  //오른손 엄지가 더 가깝거나, 거리가 동일하나 오른손잡이일 경우
+              rightPos[0] = numPos[0];
+              rightPos[1] = numPos[1];
+              result += 'R';
+          }
+      }
+  }
+  
+  return result;
+}
+
+
+
+
+/* 내 첫 풀이 */
 
 function solution(numbers, hand) {
   // 2,5,8,0 : 가까운 엄지. 거리같다면 오른손잡이 - 오른속, 왼손잡이 - 왼손
