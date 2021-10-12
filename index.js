0e44d6f6d416f62f666b2d6ad056739198ab64a7
jyy1554 <jyy1556@gmail.com>
2021-10-12 19:24:48 +0900

    Solve a bug which happens when a clock hand reaches to 12

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
+  if(seconds === 0) $secondHand.classList.add('bypass');
+  if(seconds === 1) $secondHand.classList.remove('bypass');
   const secondsDegrees = ((seconds / 60) * 360) + 90; // default가 -90도 이므로
   $secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
+  if(minutes === 0) $minuteHand.classList.add('bypass');
+  if(minutes === 1) $minuteHand.classList.remove('bypass');
   const minutesDegrees = ((minutes / 60) * 360) + 90;
   $minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
   
