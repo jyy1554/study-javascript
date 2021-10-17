df2c38e9e14aaf10bc92e1085ee504c4e5b4ef40
jyy1554 <jyy1556@gmail.com>
2021-10-18 00:23:36 +0900

    - 완료

---
+++ programmers/level2/maxAndmin.js
@@ -0,0 +1,11 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12939
+
+// 최댓값과 최솟값
+
+function solution(s) {
+  let answer = [];
+  
+  answer = s.split(" ").sort((a,b) => a-b);
+  
+  return `${answer[0]} ${answer[answer.length-1]}`;
+}
