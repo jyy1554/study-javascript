193f29f7a4629d72722aa55ee7f9fc0e550461e3
jyy1554 <jyy1556@gmail.com>
2021-10-23 22:22:02 +0900

    올바른 괄호 - 효율성테스트 fail

--- programmers/level2/parenthesis.js
+++ programmers/level2/parenthesis.js
@@ -2,18 +2,15 @@
 
 // 올바른 괄호
 
+
 function solution(s){
-  let answer = 0;
-  
-  // 첫 괄호와 마지막 괄호가 잘못되었을 경우
-  if (s[0] === ")" || s[s.length-1] == "(") return false;
-  
-  // (개수와 )개수가 같은 경우
-  for (let i=0; i < s.length; i++) {
-      if (s[i] === "(") answer++;
-      else answer--;
+    
+  while (s.length) {
+      const str = s.split("()").join("");  //()가 있으면 ()을 기준으로 분리한 배열을 return함
+      
+      if (s == str) break;
+      else s = str;
   }
   
-  
-  return answer = answer == 0 ? true : false;
+  return s.length == 0 ? true : false;
 }
