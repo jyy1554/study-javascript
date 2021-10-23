17ac7848990d6b55633c4dfa62bfde1694566267
jyy1554 <jyy1556@gmail.com>
2021-10-23 23:41:55 +0900

    올바른 괄호 - 완료

--- programmers/level2/parenthesis.js
+++ programmers/level2/parenthesis.js
@@ -4,13 +4,16 @@
 
 
 function solution(s){
-    
-  while (s.length) {
-      const str = s.split("()").join("");  //()가 있으면 ()을 기준으로 분리한 배열을 return함
-      
-      if (s == str) break;
-      else s = str;
+  const stack = [];
+  
+  if (s[0] == ")" || s[s.length-1] == "(") return false;
+  
+  for (let i=0; i < s.length; i++) {
+      if (s[i] === "(") stack.push(s.charAt(i));
+      // ")"일 경우
+      else if (stack.length == 0) return false;
+      else stack.pop();
   }
   
-  return s.length == 0 ? true : false;
+  return stack.length == 0 ? true : false;
 }
