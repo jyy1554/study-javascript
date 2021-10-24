3943c978e0cf24cace988847401251d14c26704c
jyy1554 <jyy1556@gmail.com>
2021-10-25 00:12:56 +0900

    스킬트리 - 완료

--- programmers/level2/skillTree.js
+++ programmers/level2/skillTree.js
@@ -3,25 +3,15 @@
 // 스킬 트리
 
 function solution(skill, skill_trees) {
-    var answer = 0;
-    
-    for (let i=0; i < skill_trees.length; i++) {
-        let index = -1;
-        
-        for (let j=0; j < skill_trees[i].length; j++) {
-            const new_index = skill.indexOf(skill_trees[i][j]);
-            if (new_index == -1) continue;
-            else if (index < new_index) index = new_index;
-            else {
-                index = -1;
-                break;
-            }
-        }
-        
-        console.log(index);
-        
-        answer = index != -1 ? (answer + 1) : answer;
-    }
-    
-    return answer;
+  var answer = 0;
+  
+  // skill_trees[i]을 배열로 만들고, skill에 없는 것들은 지우기
+  
+  for (let i=0; i < skill_trees.length; i++) {
+      let arr = skill_trees[i].split('').filter(v => skill.indexOf(v) != -1);
+      
+      if (arr.join("") === skill.substr(0,arr.length)) answer++;
+  }
+  
+  return answer;
 }
