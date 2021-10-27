eb311a5ea1c004ba965755bf9734a0e41bdb78e0
jyy1554 <jyy1556@gmail.com>
2021-10-28 00:11:39 +0900

    n^2 배열 자르기 - 풀이중(시간초과3)

--- programmers/level2/sliceNarray.js
+++ programmers/level2/sliceNarray.js
@@ -3,30 +3,31 @@
 // n^2 배열 자르기
 
 function solution(n, left, right) {
-  let row = [];
+  let row = 0;
   let answer = [];
   let index = 0;
   let v = 0;
   
   // n = 4
-  // [1 2 3 4]
-  // [2 2 3 4]     = 이전행 [1 2 3 4] + [1 0 0 0]
-  // [3 3 3 4]     = [2 2 3 4] + [1 1 0 0]
-  // [4 4 4 4]     = [3 3 3 4] + [1 1 1 0]
+  // 0행 : [1 2 3 4]
+  // 1행 : [2 2 3 4]     = 이전행 [1 2 3 4] + [1 0 0 0]
+  // 2행 : [3 3 3 4]     = [2 2 3 4] + [1 1 0 0]
+  // 3행 : [4 4 4 4]     = [3 3 3 4] + [1 1 1 0]
   
   while (index <= right) {
       // 첫행
       if (index < n) {
           v = index + 1;
-          row.push(v);
       }
-      // 몇행 : Math.floor(index / n)  해당행의 몇번째 요소 : index % n 
-      // 해당요소는 다음행에서 index % n < Math.floor면 1을 더함
+      // 행 : Math.floor(index / n)    
+      // n = 1(2번째행),2(3번째행),3,...
+      else if (index % n <= Math.floor(index / n)) {
+          v = Math.floor(index / n) + 1;
+      }
       else {
-          const elem = index % n;
-          v = row [elem] + (elem < Math.floor(index / n));
-          row[elem] = v;
+          v = index % n + 1;
       }
+      
       
       // left와 right 사이에 있는 요소인 경우
       if (index >= left && index <= right) answer.push(v);
@@ -36,3 +37,4 @@ function solution(n, left, right) {
   
   return answer;
 }
+
