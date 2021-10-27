c66b948fc2ac6ccac0e73783f2f72f73b9eae5a7
jyy1554 <jyy1556@gmail.com>
2021-10-27 23:00:17 +0900

    n^2 배열 자르기

---
+++ programmers/level2/sliceNarray.js
@@ -0,0 +1,34 @@
+// https://programmers.co.kr/learn/courses/30/lessons/87390
+
+// n^2 배열 자르기
+
+function solution(n, left, right) {
+  let answer = [];
+  
+  // n = 4
+  // [1 2 3 4]
+  // [2 2 3 4]     = 이전행 [1 2 3 4] + [1 0 0 0]
+  // [3 3 3 4]     = [2 2 3 4] + [1 1 0 0]
+  // [4 4 4 4]     = [3 3 3 4] + [1 1 1 0]
+  
+  
+  for (let i=0; i < n; i++) {
+      if (i == 0) {
+          for (let j=1; j <= n; j++) {
+              answer.push(j);
+          }
+      }
+      else {
+          const arr = answer.slice(answer.length-n, answer.length);
+          for (let j=0; j < n; j++) {
+              if (j < i) answer.push(arr[j] + 1);
+              else answer.push(arr[j]);
+          }
+      }
+      
+      if (answer.length > right) break;
+  }
+
+  return answer.slice(left, right+1);
+}
+
