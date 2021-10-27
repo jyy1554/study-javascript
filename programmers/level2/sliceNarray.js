2d75081bb41afd0085936e702e5a5865af5cb267
jyy1554 <jyy1556@gmail.com>
2021-10-28 01:25:31 +0900

    n^2 배열 자르기 - 완료

--- programmers/level2/sliceNarray.js
+++ programmers/level2/sliceNarray.js
@@ -3,38 +3,17 @@
 // n^2 배열 자르기
 
 function solution(n, left, right) {
-  let row = 0;
-  let answer = [];
-  let index = 0;
-  let v = 0;
+  // 123
+  // 223
+  // 333
+  // 행렬의 (row, column)번째 요소값은 max(row, column) + 1이다.
+  const answer = [];
   
-  // n = 4
-  // 0행 : [1 2 3 4]
-  // 1행 : [2 2 3 4]     = 이전행 [1 2 3 4] + [1 0 0 0]
-  // 2행 : [3 3 3 4]     = [2 2 3 4] + [1 1 0 0]
-  // 3행 : [4 4 4 4]     = [3 3 3 4] + [1 1 1 0]
-  
-  while (index <= right) {
-      // 첫행
-      if (index < n) {
-          v = index + 1;
-      }
-      // 행 : Math.floor(index / n)    
-      // n = 1(2번째행),2(3번째행),3,...
-      else if (index % n <= Math.floor(index / n)) {
-          v = Math.floor(index / n) + 1;
-      }
-      else {
-          v = index % n + 1;
-      }
-      
-      
-      // left와 right 사이에 있는 요소인 경우
-      if (index >= left && index <= right) answer.push(v);
-      
-      index++;
+  for (let i=left; i <= right; i++) {
+      const row = Math.floor(i / n);
+      const column = i % n;
+      answer.push(Math.max(row, column) + 1);
   }
-  
+
   return answer;
 }
-
