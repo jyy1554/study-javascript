7a8e6f6121e1602a4aaf0ddd51ff906fe6eb1d06
jyy1554 <jyy1556@gmail.com>
2021-10-29 01:27:02 +0900

    Learn how to deal with array method

--- index.js
+++ index.js
@@ -28,31 +28,67 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-console.log(inventors.filter(inventor => inventor.year >= 1500 && inventor.year <= 1600));
+const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600));
+console.table(fifteen);
+
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
-console.log(inventors.map(inventor => [inventor.first, inventor.last]));
+const fullNames1 = inventors.map(inventor => inventor.first + ' ' + inventor.last);
+const fullNames2 = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullNames1, fullNames2);
+
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
-console.log(inventors.sort((a,b) => a.year - b.year));
+const ordered = inventors.sort((a,b) => a.year > b.year ? 1 : -1);
+console.table(ordered);
+
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
-console.log(inventors.reduce((acc, cur, i) => acc += cur.passed - cur.year, 0));
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYears);
+
 
 // 5. Sort the inventors by years lived
-console.log(inventors.sort((a,b) => (a.passed - a.year) - (b.passed - b.year)));
+const oldest = inventors.sort((a,b) => (a.passed - a.year) > (b.passed - b.year) ? -1 : 1);
+console.table(oldest);
+
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// 아래 위키에서 de가 포함되어있는 Boulevards를 가져오라는 의미
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a')); // link로 되어있는 것들만 뽑기. 그러나 array가 아닌 NodeList를 리턴하므로 array로 바꿔줘야됨
+// // const linkes = [...category.querySelectorAll('a')]; // 이렇게도 가능
+// const de = links.map(link => link.textContent)
+//                 .filter(streetName => streetName.includes('de')); // list로 만들어줄꺼이므로 map 사용
 
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(', ');
+  const [bLast, bFirst] = nextOne.split(', ');
+  return aLast > bLast ? 1 : -1;
+});
+console.table(alpha);
 
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+const tarnsportations = data.reduce((obj, item) => {
+  if(!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(tarnsportations);
