91363ef53d15141da699125f3424396e3d02ebc4
jyy1554 <jyy1556@gmail.com>
2021-10-30 23:37:36 +0900

    Complete the css file

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // 클릭할때마다 class name을 add하거나 없애야됨
 
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  // toggle은 class 유무를 체크해서 없으면 add, 있으면 remove를 자동으로 시켜줌
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);  // 어떤 transition이 일어나나 확인하기 위해
+  if(e.propertyName === 'font-size') this.classList.toggle('open-active');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
