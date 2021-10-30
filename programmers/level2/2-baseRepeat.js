53ba83812ee8a4aece4ad3278a9e9acc2e779c41
jyy1554 <jyy1556@gmail.com>
2021-10-30 23:58:54 +0900

    이진 변환 반복하기 - 풀이중

---
+++ programmers/level2/2-baseRepeat.js
@@ -0,0 +1,19 @@
+// https://programmers.co.kr/learn/courses/30/lessons/70129
+
+// 이진 변환 반복하기
+
+function solution(s) {
+  let answer = [0,0];
+  let num = s;
+  
+  // n : 이진변환 횟수,   z : 제거된 0의 개수
+  while (num != '1') {
+      const new_num = s.split("").filter(v => +v != 0).join("");
+      answer[0]++;
+      answer[1] += num.length - new_num.length;
+      
+      num = (new_num.length).toString(2);
+  }
+  
+  return answer;
+}
