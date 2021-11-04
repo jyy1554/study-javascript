86d9e42a3ef997cd8a18bc7729d4215345fa6413
jyy1554 <jyy1556@gmail.com>
2021-11-04 22:10:03 +0900

    A+B - 7 : 완료

--- Baekjoon/IO/11021.js
+++ Baekjoon/IO/11021.js
@@ -2,3 +2,12 @@
 
 // A+B - 7
 
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const len = input.shift();
+for(let i=0; i < len; i++) {
+    const [a,b] = input[i].split(' ');
+    
+    console.log(`Case #${i+1}: ${(+a)+(+b)}`);
+}
