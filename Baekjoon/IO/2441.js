ae9a06fcb4616fb49cc86971382dffc460f8411a
jyy1554 <jyy1556@gmail.com>
2021-11-05 21:19:15 +0900

    별 찍기34 - 완료

---
+++ Baekjoon/IO/2441.js
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/2441
+
+// 별 찍기 - 4
+
+const fs = require('fs');
+const input = +fs.readFileSync('/dev/stdin').toString();
+
+for (let i=input; i > 0; i--) {
+    console.log(' '.repeat(input-i) + '*'.repeat(i));
+}
