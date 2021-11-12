6b4dc3a370e8a27966a75cdc66b415744d602154
jyy1554 <jyy1556@gmail.com>
2021-11-12 23:57:33 +0900

    쉬운 계단 수  - 완료

---
+++ Baekjoon/DP/10844.js
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/10844
+
+// 쉬운 계단 수
+
+const fs = require('fs');
+const N = +fs.readFileSync('/dev/stdin').toString();
+
+const dp = [0, 9];
+
+for (let i=2; i <= N; i++) {
+  dp[i] = dp[i-1] * 2 - (i-1);
+}
+
+console.log(dp[N]);
