a46ddb39ba36b318db5bf293afbb7a6f0585348b
jyy1554 <jyy1556@gmail.com>
2021-11-12 23:58:33 +0900

    쉬운 계단 수  - 풀이중

--- Baekjoon/DP/10844.js
+++ Baekjoon/DP/10844.js
@@ -8,7 +8,7 @@ const N = +fs.readFileSync('/dev/stdin').toString();
 const dp = [0, 9];
 
 for (let i=2; i <= N; i++) {
-  dp[i] = dp[i-1] * 2 - (i-1);
+  dp[i] = (dp[i-1] * 2 - (i-1)) % 1,000,000,000;
 }
 
 console.log(dp[N]);
