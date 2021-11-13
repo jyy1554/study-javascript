54f66699fa5cd8f12338620baca4bfb738c2a3dc
jyy1554 <jyy1556@gmail.com>
2021-11-13 12:33:38 +0900

    쉬운 계단 수  - 완료

--- Baekjoon/DP/10844.js
+++ Baekjoon/DP/10844.js
@@ -5,10 +5,28 @@
 const fs = require('fs');
 const N = +fs.readFileSync('/dev/stdin').toString();
 
-const dp = [0, 9];
+let arr = [];
 
-for (let i=2; i <= N; i++) {
-  dp[i] = (dp[i-1] * 2 - (i-1)) % 1,000,000,000;
+//2차 배열 만들기
+for (let i=0; i < N; i++) {
+  arr.push(Array(10).fill(0));
 }
 
-console.log(dp[N]);
+//N이 1일때,
+for (let i=1; i < 10; i++) {
+  arr[0][i] = 1;
+}
+
+for (let i=1; i < N; i++) {
+  arr[i][0] = arr[i-1][1];
+  arr[i][9] = arr[i-1][8];
+  for (let j=1; j <= 8; j++) {
+    arr[i][j] = (arr[i-1][j-1] + arr[i-1][j+1]) % 1000000000;
+  }
+}
+
+const sum = arr[N - 1].reduce((acc, cur) => {
+    return (acc + cur) % 1000000000;
+}, 0);
+
+console.log(sum);
