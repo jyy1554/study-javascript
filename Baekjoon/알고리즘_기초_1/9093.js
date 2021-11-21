af75cbf6e397ee129025177acbd56ed3a9169a53
jyy1554 <jyy1556@gmail.com>
2021-11-21 19:44:54 +0900

    단어 뒤집기 - 완료(풀이 2가지)

--- Baekjoon/알고리즘_기초_1/9093.js
+++ Baekjoon/알고리즘_기초_1/9093.js
@@ -2,6 +2,9 @@
 
 // 단어 뒤집기
 
+
+// 스택을 사용하지 않은 풀이
+
 const inputs = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
 const len = inputs.shift();
@@ -12,3 +15,26 @@ for (let i=0; i < len; i++) {
 }
 
 console.log(result.join("\n"));
+
+
+// 스택을 사용한 풀이
+
+const inputs = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+const len = inputs.shift();
+for (let i=0; i < len; i++) {
+    let answer = '';
+    const stack = [];
+    
+    inputs[i].split(" ").forEach(word => {
+        for (let j=0; j < word.length; j++) {
+            stack.push(word[j]);
+        }
+        for (let j=0; j < word.length; j++) {
+            answer += stack.pop();
+        }
+        answer += ' ';
+    });
+    
+    console.log(answer);
+}
