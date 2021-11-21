a63dbeec32684d3ea3619a8f6351e06d8b0176af
jyy1554 <jyy1556@gmail.com>
2021-11-21 19:30:45 +0900

    단어 뒤집기 - 완료2

--- Baekjoon/알고리즘_기초_1/9093.js
+++ Baekjoon/알고리즘_기초_1/9093.js
@@ -5,10 +5,10 @@
 const inputs = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
 const len = inputs.shift();
-const queue = [];
+const result = [];
 
 for (let i=0; i < len; i++) {
-  inputs[i].split(" ").forEach(word => queue.push(word.split("").reverse().join("")));
+  inputs[i].split(" ").forEach(word => result.push(word.split("").reverse().join("")));
 }
 
-console.log(queue.join("\n"));
+console.log(result.join("\n"));
