a8679b1a3ca7858e10de1c89c8ea6c374ff42691
jyy1554 <jyy1556@gmail.com>
2021-11-21 20:22:58 +0900

    괄호 - 완료(풀이 2가지)

--- Baekjoon/알고리즘_기초_1/9012.js
+++ Baekjoon/알고리즘_기초_1/9012.js
@@ -2,6 +2,9 @@
 
 // 괄호
 
+
+// 스택을 이용한 풀이
+
 const inputs = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 const len = inputs.shift();
 
@@ -21,4 +24,21 @@ for (let i=0; i < len; i++) {
   }
 
   VPS && !stack.length ? console.log("YES") : console.log("NO");
+}
+
+// 스택을 이용하지 않은 풀이
+const inputs = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const len = inputs.shift();
+
+for (let i=0; i < len; i++) {
+  let count = 0;
+    
+  for (let j=0; j < inputs[i].length; j++) {
+    if (inputs[i][j] == "(") count++;
+    else count--;
+      
+    if (count < 0) break;
+  }
+    
+  count === 0 ? console.log("YES") : console.log("NO");
 }
