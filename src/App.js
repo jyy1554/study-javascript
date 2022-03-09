897ef9cda3c6c2492ee8fa215faa30f57d831e64
jyy1554 <jyy1556@gmail.com>
2022-03-09 22:07:07 +0900

    d3로 창 크기에 맞게 변하는 Responsive Chart 그리기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import Interactivity from './components/Interactivity';
+import React, { useState } from 'react';
+import ResponsiveChart from './components/ResponsiveChart';
 import './App.css';
 
 
 function App() {
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
+
   return (
     <React.Fragment>
-      <Interactivity />
-    </React.Fragment>
+      <ResponsiveChart data={data} />
+      <button onClick={() => setData(data.map(value => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter(value => value <= 35))}>
+        Filter data
+      </button>
+      <button onClick={() => setData([...data, Math.round(Math.random() * 100)])}>
+        Add data
+      </button>
+  </React.Fragment>
   );
 }
 
