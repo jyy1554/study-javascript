52623c5bc70c683aec928c656130dd3f82327cb1
jyy1554 <jyy1556@gmail.com>
2022-03-09 18:35:51 +0900

    d3로 animated bar 그리기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import AxesAndScales from './components/AxesAndScales';
+import AnimatedBar from './components/AnimatedBar';
 import './App.css';
 
 
 function App() {
   return (
     <React.Fragment>
-      <AxesAndScales />
+      <AnimatedBar />
     </React.Fragment>
   );
 }
