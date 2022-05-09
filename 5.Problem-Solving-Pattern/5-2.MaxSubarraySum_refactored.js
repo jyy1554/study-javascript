af5b7e9419a901aeb2bff0af992066dbf87eaae7
jyy1554 <jyy1556@gmail.com>
2022-05-10 00:21:18 +0900

    기준점 간 이동 배열 패턴(Sliding Window) 학습_수정

--- 5.Problem-Solving-Pattern/5-2.MaxSubarraySum_refactored.js
+++ 5.Problem-Solving-Pattern/5-2.MaxSubarraySum_refactored.js
@@ -1,3 +1,6 @@
+//기준점 간 이동배열(Sliding Window) 패턴
+//array안에 n개의 요소들을 묶어 나오는 합들 중 가장 큰 합을 구하라
+
 console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)); //19
 
 function maxSubarraySum(arr, num) {
