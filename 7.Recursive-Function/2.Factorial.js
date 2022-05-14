ec970e8f6cc37dce2143e9855abe2f5df7f93419
jyy1554 <jyy1556@gmail.com>
2022-05-14 23:19:42 +0900

    재귀함수 기초 공부

---
+++ 7.Recursive-Function/2.Factorial.js
@@ -0,0 +1,14 @@
+function factorial(num) {
+    if (num === 1) return 1;
+    return num * factorial(num - 1);
+}
+
+// function factorial(num) {
+//     let total = 1;
+//     for (let i = num; i > 0; i--) {
+//         total *= i;
+//     }
+//     return total;
+// }
+
+console.log(factorial(5));
