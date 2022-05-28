3e6fc4e22d571c69c97319f63cc589b2ca565a17
jyy1554 <jyy1556@gmail.com>
2022-05-28 11:10:42 +0900

    단일 연결 리스트(Singly Linked List) shift, unshift 메소드 구현

--- 10.Data-Structures/1.SinglyLinkedList.js
+++ 10.Data-Structures/1.SinglyLinkedList.js
@@ -61,6 +61,18 @@ class SinglyLinkedList {
         }
         return currentHead;
     }
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 let list = new SinglyLinkedList();
