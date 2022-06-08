a509524921095a227297ef25f709990f17482407
jyy1554 <jyy1556@gmail.com>
2022-06-08 22:14:55 +0900

    Doubly Linked List 이중 연결 리스트 pop 메소드 구현

--- 10.Data-Structures/2.DoublyLinkedList.js
+++ 10.Data-Structures/2.DoublyLinkedList.js
@@ -1,32 +1,59 @@
+// Node : val, next, prev
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-        this.prev = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
 }
-// let first = new Node(12);
-// first.next = new Node(13);
 
+// DoublyLinkedList : head, tail, length
 class DoublyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-    push(val) {
-        let newNode = new Node(val);
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
-        }
+  push(val) {
+    let newNode = new Node(val);
 
-        this.length++;
-        return this;
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
+
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let poppedNode = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null; //뒤늦게 추가
+    }
+
+    this.length -= 1;
+    return poppedNode;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+  }
 }
