568babf8c7cd6ce5d5e12fecf4bd4863dca8d247
jyy1554 <jyy1556@gmail.com>
2022-06-08 23:05:42 +0900

    Doubly Linked List 이중 연결 리스트 shift, unshift, get 메소드 구현

--- 10.Data-Structures/2.DoublyLinkedList.js
+++ 10.Data-Structures/2.DoublyLinkedList.js
@@ -16,7 +16,7 @@ class DoublyLinkedList {
   }
 
   push(val) {
-    let newNode = new Node(val);
+    const newNode = new Node(val);
 
     if (this.length === 0) {
       this.head = newNode;
@@ -36,7 +36,7 @@ class DoublyLinkedList {
       return undefined;
     }
 
-    let poppedNode = this.tail;
+    const poppedNode = this.tail;
 
     if (this.length === 1) {
       this.head = null;
@@ -47,13 +47,69 @@ class DoublyLinkedList {
       poppedNode.prev = null; //뒤늦게 추가
     }
 
-    this.length -= 1;
+    this.length--;
     return poppedNode;
   }
 
   shift() {
-    if (!this.head) {
+    if (this.length === 0) {
       return undefined;
     }
+
+    const oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    const newHead = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newHead;
+      this.tail = this.head;
+    } else {
+      this.head.prev = newHead;
+      newHead.next = this.head; //헷갈림
+      this.head = newHead;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    let currentNode = this.head;
+    let count = 0;
+
+    if (index <= this.length / 2) {
+      while (index !== count) {
+        currentNode = currentNode.next;
+        count++;
+      }
+    } else {
+      currentNode = this.tail;
+      count = this.length - 1;
+
+      while (index !== count) {
+        currentNode = currentNode.prev;
+        count--;
+      }
+    }
+
+    return currentNode;
   }
 }
