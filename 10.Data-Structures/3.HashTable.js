73f05d57708d052012d0ea404b2fe710479928bc
jyy1554 <jyy1556@gmail.com>
2022-06-11 21:08:12 +0900

    Hash Table에서 set 메소드 구현

--- 10.Data-Structures/3.HashTable.js
+++ 10.Data-Structures/3.HashTable.js
@@ -1,14 +1,36 @@
-function hash(key, arrayLen) {
-  let total = 0;
-
-  for (const char of key) {
-    const value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
 
-  return total;
+  _hash(key) {
+    let total = 0;
+    const WEIRD_PRIME = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const char = key[i];
+      const value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+
+    return total;
+  }
+
+  set(key, value) {
+    // const arr = [key, value];
+    const index = this._hash(key);
+
+    // [, , , , [[key, value]], , ] -> [, , , [[key, value], [key, value]], , ]
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+
+    this.keyMap[index].push([key, value]);
+  }
 }
 
-console.log(hash("pink", 10)); // 0
-console.log(hash("orangered", 10)); // 7
-console.log(hash("cyan", 10)); // 3
+const ht = new HashTable();
+ht.set("hello world", "goodbye!!");
+ht.set("dogs", "are cool");
+ht.set("cats", "are fine");
+ht.set("i love", "pizza");
