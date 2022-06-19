bd927554561e4ab870a5ba09f791a3e472bc0bc8
jyy1554 <jyy1556@gmail.com>
2022-06-19 18:52:58 +0900

    Solve LeetCode 141. Linked List Cycle

---
+++ LeetCode/Linked-List/141.Linked-List-Cycle.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  // cycle이 있는지 판단하라 : next로 계속 접근가능 (tail에 연결)
+  //null -> false / 만나면 -> true
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+
+  return false;
+};
