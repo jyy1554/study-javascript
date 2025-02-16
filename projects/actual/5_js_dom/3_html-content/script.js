65a71cf4269be0df83bbeb1095d1399ccbe11929
jyy1554 <jyy1556@gmail.com>
2025-02-16 16:22:14 +0900

    innerHTML

---
+++ projects/actual/5_js_dom/3_html-content/script.js
@@ -0,0 +1,8 @@
+const playground = document.getElementById('playground');
+
+const spellbook = document.createElement('div');
+spellbook.innerHTML = '<p>Spells include: <strong>Levitation</strong> and <em>Invisibility</em>!</p>';
+playground.append(spellbook);
+
+// const userInput = '<img src="x" onerror="alert(\'Hacked!\')">';
+// playground.innerHTML = userInput;
